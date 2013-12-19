49e5a779c2cdac3f0a62930a796ca160d355e318
Katelyn Slater <ktaeyln@gmail.com>
2013-12-19 11:36:17 -0500

    Renamed Users category to Account Renamed Users section to Accounts [ControlPanel]

---
+++ Account/Users ControlPanel/boot.inc.php
@@ -0,0 +1,6 @@
+<?php
+ControlPanel::registerPage("Accounts", "Create", "edit/create-user.json", true, 0, 1, true);
+ControlPanel::registerPage("Accounts", "Manage", "edit/manage-users.json", true, 0);
+ControlPanel::registerPage("Accounts", "Edit User", "edit/edit-user.json", false);
+ControlPanel::registerPage("Accounts", "Set User Password", "edit/set-user-password.inc.php", false);
+?>
