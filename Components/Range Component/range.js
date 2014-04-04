c47823a13760ae725456388456a805e583a84ea6
Katelyn Slater <ktaeyln@gmail.com>
2014-04-04 15:04:12 -0400

    Added single mode and disabled state to Range Component Improved theme of Range Component

--- Components/Range Component/range.js
+++ Components/Range Component/range.js
@@ -1,7 +1,7 @@
 Framework.Components.registerComponent("range", {
 	
-	getValue: function() {
-		return this.values;
+	getValue: function(full) {
+		return !full && this.singleMode ? this.values[1] : this.values;
 	},
 	
 	getMinimum: function() {
@@ -14,17 +14,22 @@ Framework.Components.registerComponent("range", {
 	
 	setMinimum: function(min) {
 		this.min = min;
+		this.values[0] = Math.min(this.values[0], this.min);
 		this.updateValues();
 	},
 	
 	setMaximum: function(max) {
 		this.max = max;
+		this.values[1] = Math.max(this.values[1], this.max);
 		this.updateValues();
 	},
 	
 	setValue: function(val) {
+		if(!Object.isArray(val))
+			val = [this.min,val*1];
+	
 		if(this.oldValues[0] == val[0] && this.oldValues[1] == val[1]) {
-			this.updateLayout();
+			this.updateLayout(this.getElement());
 			return; // No update
 		}
 		
@@ -33,19 +38,30 @@ Framework.Components.registerComponent("range", {
 		this.updateValues();
 	},
 	
-	updateLayout: function() {
-		var el = this.getElement();
-		var width = el.measure("width")-(this.knobSize*2);
+	updateLayout: function(el) {
+		var width;
+		if(this.singleMode) {
+			width = el.measure("width")-this.knobSize;
+		} else {
+			width = el.measure("width")-(this.knobSize*2);
+		}
 		var minPerc = ((this.values[0] - this.min) / this.valMod);
 		var maxPerc = ((this.values[1] - this.min) / this.valMod);
 		this.minKnob.slideTo({
 			"left": (minPerc*width) + "px"
 		});
-		this.maxKnob.slideTo({
-			"left": (this.knobSize+(maxPerc*width)) + "px"
-		});
-		var left = minPerc*width;
-		var right = (maxPerc*width)+this.knobSize;
+		if(this.singleMode)
+			this.maxKnob.slideTo({
+				"left": (maxPerc*width) + "px"
+			});
+		else
+			this.maxKnob.slideTo({
+				"left": (this.knobSize+(maxPerc*width)) + "px"
+			});
+		var left = this.singleMode ? 0 : minPerc*width;
+		var right = (maxPerc*width);
+		if(!this.singleMode)
+			right += this.knobSize;
 		this.bar.slideTo({
 			"left": left + "px"
 		});
@@ -72,9 +88,25 @@ Framework.Components.registerComponent("range", {
 		this.tick = el.readAttribute("tick")*1 || 1;
 		this.valMod = this.max-this.min;
 		
+		this.singleMode = el.hasAttribute("single");
+		if(this.singleMode)
+			this.minKnob.hide();
+		else
+			this.minKnob.show();
+			
+		this.disabled = el.hasAttribute("disabled");
+		if(this.disabled)
+			el.addClassName("disabled");
+		else
+			el.removeClassName("disabled");
+		
 		this.values = el.readAttribute("value");
 		if(this.values) {
 			this.values = this.values.split(/[,\s]+/);
+			if(!this.values[1]) {
+				this.values[1] = this.values[0];
+				this.values[0] = this.min;
+			}
 			this.values = [this.values[0]*1,this.values[1]*1];
 		} else
 			this.values = [this.min,this.max];
@@ -101,6 +133,14 @@ Framework.Components.registerComponent("range", {
 		this.makeDraggable(this.minKnob);
 		this.makeDraggable(this.maxKnob);
 		
+		Event.on(this.minKnob, "drag:start", (function(e) {
+			if(this.disabled)
+				e.stop(); // Prevent event
+		}).bind(this));
+		Event.on(this.maxKnob, "drag:start", (function(e) {
+			if(this.disabled)
+				e.stop(); // Prevent event
+		}).bind(this));
 		Event.on(this.minKnob, "drag:move", (function(e) {
 			var pos = e.memo;
 			//console.log(pos);
@@ -127,7 +167,10 @@ Framework.Components.registerComponent("range", {
 			//console.log(pos);
 		
 			var width = el.measure("width");
-			pos.left = Math.max(this.knobSize, pos.left);
+			if(this.singleMode)
+				pos.left = Math.max(0, pos.left);
+			else
+				pos.left = Math.max(this.knobSize, pos.left);
 			pos.left = Math.min(width-this.knobSize, pos.left);
 			this.maxKnob.setStyle({"left": pos.left + "px"});
 			width -= this.knobSize*2;
@@ -147,7 +190,7 @@ Framework.Components.registerComponent("range", {
 		Event.on(this.minKnob, "drag:stop", (function(e) {
 			var val = this.minKnob.measure("left");
 			val /= el.measure("width")-(this.knobSize*2);
-			console.log(val);
+			//console.log(val);
 			val *= this.valMod;
 			val += this.min;
 			
@@ -159,9 +202,13 @@ Framework.Components.registerComponent("range", {
 		Event.on(this.maxKnob, "drag:stop", (function(e) {
 			var val = this.maxKnob.measure("left");
 			
-			val -= this.knobSize;
-			val /= el.measure("width")-(this.knobSize*2);
-			console.log(val);
+			if(this.singleMode)
+				val /= el.measure("width")-this.knobSize;
+			else {
+				val -= this.knobSize;
+				val /= el.measure("width")-(this.knobSize*2);
+			}
+			//console.log(val);
 			val *= this.valMod;
 			val += this.min;
 			
